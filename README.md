2203183deb133c81d67a17d2db83a91e9c1b9e1b
Stacy Burris <stacy1burris@gmail.com>
2020-09-30 18:17:47 -0700

    added question 6, 7 and final question

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# about-me-project
+# Lab - 02
 
-## 201 lab 2 about me guessing game
+## About me guessing game
 
-+ Hi my name is Stacy and I am a student at CodeFellows working toward becoming a software developer!
++ This page gives a user a small biography about me and asks them "yes" or "no" questions through prompts and alerts to tell them if they are correct or not.
+
++ Added a 6th and 7th question. One is a guessing the number of years ive lived in seattle and the other is using an array and having the user guess my favorite ice cream flavors.
+
++ Lastly I added an alert to tell the user how many questions they got wrong or right to show them how well they know me!
+
+### Author: Stacy
+
+### Links and resources
+
+### Reflection
+
++ I used what we learned in class today to create if/else statements and created a quiz game using information in my biography.
+
++ We learned arrays and how to use them to have the user guess an answer but only give them x chances.
--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 'use strict'
 
+var numberCorrect = 0
+// greeting question
 var userName = prompt('Hi there, welcome to my webpage! What is your name?')
 
 console.log('The user\'s name: ' + userName);
 
 alert('It\'s lovely to meet you, ' + userName + '!');
 
-
+// first question
 var whichSchool = prompt( userName + ', I have a few questions for you so you can better get to know me, do you think I went to Seattle University?');
 
 if(whichSchool.toLowerCase() === 'yes' || whichSchool.toLowerCase() === 'y'){
 
   alert('Correct! I studied forensic Psychology, but only for a year');
+  numberCorrect++
 
 } else {
 
@@ -20,32 +23,35 @@ if(whichSchool.toLowerCase() === 'yes' || whichSchool.toLowerCase() === 'y'){
 console.log('User can guess if I went to Seattle U or not');
 
 
-
+// second question
 var workLife = prompt('Did you know I was a skydive instructor?? ');
 
 if(workLife.toUpperCase() === 'No' || workLife.toUpperCase() === 'N'){        
     alert('Sike! I\'m afraid of heights and dark water which you could encounter both on that type of work life style. Am I right?! ');
 } else {
-    alert('Correct! I had a normal job, Wouldn\'t that be cool if I was!');   
+    alert('I had a normal job, Wouldn\'t that be cool if I was!'); 
+    numberCorrect++  
 } 
 console.log('User guesses if I was a skydive instructor or not');
 
-
+// third question
 var workHistory = prompt('Wanna know what my past job was and what I\'m doing now? ');
 
 if(workHistory.toLowerCase() === 'No' || workHistory.toLowerCase() === 'N'){
     alert('Great, I worked at a Salon on Capitol hill for 8+ years and now going to school at Code Fellows to become a Software Developer!');
+    numberCorrect++
 } else {
     alert('Well I\'ll tell you anyways, I worked at a salon in Seattle and now I\'m in school at Code Fellows to become a Software Developer!');
 }
 console.log('User finds out past work history plus current schooling');
 
-
+// forth question
 var lifeGoals = prompt('Id love to share with you about my future goals, think I\'ll make a good Software Developer?');
 
 if(lifeGoals.toLowerCase() === 'y' || lifeGoals.toLowerCase() === 'yes'){
 
     alert('Aww thats sweet that you think so ' + userName + ', Me too! fingers crossed I make it through!');
+    numberCorrect++
 
     console.log('If yes they learn about my future goals in a nice way');
 
@@ -56,13 +62,13 @@ if(lifeGoals.toLowerCase() === 'y' || lifeGoals.toLowerCase() === 'yes'){
     console.log('If no user gets message about proving them wrong');
 }
 
-
+// fifth question
 var funLanguageFact = prompt('I\'m in the beginning of learning a new language, Wanna know what it is?');
 
 if(funLanguageFact.toLowerCase() === 'y' || funLanguageFact.toLowerCase() === 'yes'){
     
     alert('Great! My boyfriend is from Gambia, West Africa so I am learning his language which is Wolof so that I can better communicate with his family next time we visit!' + ' Well ' + userName + ',' + ' Thanks for visiting and getting to know me better, Have a nice day!');
-
+    numberCorrect++
     console.log('If yes, the user will find out what language I am learning');
 
 } else if(funLanguageFact.toUpperCase() == 'n' || funLanguageFact.toUpperCase() == 'no'){
@@ -72,7 +78,7 @@ if(funLanguageFact.toLowerCase() === 'y' || funLanguageFact.toLowerCase() === 'y
     console.log('If no, then I am disappointed and say goodbye');
 
 }
-
+// guess a number question
 var correctAgeNumber = 28;
 for (var i = 0; i < 4; i++) {
   var numberGuess = prompt('Guess how long ive lived in Seattle!');
@@ -84,6 +90,7 @@ for (var i = 0; i < 4; i++) {
   }
     else if (parseInt(numberGuess) === correctAgeNumber) {
     alert('That is correct 28 years!');
+    numberCorrect++
     break;
    }
 if (i === 27 ){
@@ -91,7 +98,39 @@ if (i === 27 ){
 }
 }
 
-  
+// array favorite flavor question
+var correctFlavor = ['chocolate peanut butter', 'neapolitan', 'cookie dough'];
+var userAnswer = '';
+var i = 1;
+
+while (i < 6){
+    var flavorQuestion = prompt('What are my favorite flavors of ice cream?');
+    for (var j = 0; j < correctFlavor.length; j++) {
+        if (flavorQuestion.toLowerCase() === correctFlavor[j]) {
+            alert('Delicious! That is correct!');
+            numberCorrect++
+            i = 8; 
+           break;
+        } 
+    }
+    if (i < 6){
+        userAnswer = flavorQuestion
+        i++ 
+        console.log(i);
+        alert('Nope! ' + userAnswer + ' is incorrect but still delicious');   
+    }        
+}
+if ( i === 6){
+    alert('You\'re out of attempts, here are all the correct answers chocolate peanut butter, neapolitan, cookie dough!');
+}
+
+// closing question
+
+if (numberCorrect > 5) {
+    alert(userName + ', thanks for visiting and getting to know me! You scored ' + numberCorrect + ' out of 7! Have a great day!') 
+} else {
+    alert(userName + ', thanks for visiting but you only scored ' + numberCorrect + ' out of 7! Have a great day!');
+}
 
 
 
